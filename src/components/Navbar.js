e4326f0fd6e9a5c6506e2381b966cc7786312d26
soumitaghosh-dsi <soumitaghosh.dsi@gmail.com>
2020-01-13 16:55:04 +0600

    hover done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,9 @@ import './NavBar.css';
 export class Navbar extends Component {
     render() {
         return (
-<<<<<<< HEAD
-            <div >
-                <nav className="main-container">
-                    <h1 className="brand"><a href="" id="mainn">DesignBoost</a></h1>
-=======
             <div className="main-container">
                 <nav>
                     <h1><a href="" id="mainn">DesignBoost</a></h1>
->>>>>>> 7296e65e449e02805b4e2b49de1d5648521bf69c
                     <ul>
                         <li>HOME</li>
                         <li>ABOUT</li>
